463d7a4474e75cd146d7c80e7da31aa1c31d4d64
Alexande B <abobrikovich@gmail.com>
2021-11-18 16:59:22 +0100

    #120 remove idnits installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -13,12 +13,6 @@ command -v cpanm >/dev/null 2>&1 || {
 }
 cpanm --notest XML::LibXSLT@1.96 git://github.com/brucemiller/LaTeXML.git@9a0e7dc5
 
-# Install idnits & xml2rfc
+# Install xml2rfc
 apt-get -y install python3-pip python3-setuptools python3-wheel
-# pip3 install --upgrade pip
 pip3 install idnits xml2rfc --ignore-installed six chardet
-
-export IDNITS_URL=https://tools.ietf.org/tools/idnits/
-export IDNITS_VER=$(curl -Ls $IDNITS_URL | grep -e 'tgz' | sed -e 's/.*\(idnits-.*\).tgz.*/\1/') ; echo ${IDNITS_VER}
-curl -SL ${IDNITS_URL}${IDNITS_VER} | tar xzv
-export PATH=$(pwd)/${IDNITS_VER}:${PATH}
