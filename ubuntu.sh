e9d50d80169fbf4d9e0646506db85cbc145f8480
Alexande B <abobrikovich@gmail.com>
2022-01-11 16:46:58 +0100

    #127 remove latexml installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,12 +7,6 @@ apt-get install -y curl git make gcc ruby-bundler ruby-dev \
 
 curl -L "https://raw.githubusercontent.com/metanorma/plantuml-install/master/ubuntu.sh" | bash
 
-# Install latexml
-command -v cpanm >/dev/null 2>&1 || {
-  curl -L http://cpanmin.us | perl - App::cpanminus
-}
-cpanm --notest XML::LibXSLT@1.96 git://github.com/brucemiller/LaTeXML.git@9a0e7dc5
-
 # Install xml2rfc
 apt-get -y install python3-pip python3-setuptools python3-wheel
 pip3 install idnits xml2rfc --ignore-installed six chardet
